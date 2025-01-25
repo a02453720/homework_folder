8f905299c131c0fd3f34c9f57c790f96e9f8a4f4
root <root@ip-172-31-8-21.us-west-1.compute.internal>
2025-01-25 00:57:41 +0000

    adding homework 2

---
+++ homework2.py
@@ -0,0 +1,37 @@
+
+# Problem 2.3
+if grade >= 90:
+    print("Congratulations! Your grade of", grade, "earns you an A")
+
+#Problem 2.4
+print(27+2)
+print(27-2)
+print(27/2)
+print(27*2)
+print(27//2)
+print(27**2)
+
+#Problem 2.5
+r = 2
+pi = 3.14159
+diameter = 2*r
+area = pi*(r**2)
+circumference = diameter*pi
+
+print(diameter, area, circumference)
+
+#Problem 2.6
+if myInt % 2 == 0:
+    print("This nubmer is even")
+
+
+#Problem 2.7
+if 1024 % 4 == 0:
+    #1024 is a multiple of 4
+if 2 % 10 == 0
+    #2 is a multiple of 10
+
+#Problem 2.8
+print("Number\tSquare\tCube\t")
+for i in range(6):
+    print(str(i) + "\t" + str(i**2) + "\t" + str(i**3))
